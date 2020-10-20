3a9bba52d78cef88bf54ab99f8e39efcc8142ebd
Takasakiii <lucasmc2709@live.com>
2020-10-19 21:55:47 -0300

    so comit

--- src/controllers/test/test.controller.ts
+++ src/controllers/test/test.controller.ts
@@ -1,11 +1,11 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Session } from "@nestjs/common";
 import { DiscordBotService } from "src/extension-modules/discord/discord-bot.service";
 
 @Controller('test')
 export default class TestController{
     constructor(private readonly discordBot: DiscordBotService){}
     @Get()
-    async index(){
-        return this.discordBot.getUser('203713369927057408')
+    async index(@Session() { user }: Express.Session){
+        return user
     }
 }
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, HttpException, HttpStatus, Post } from "@nestjs/common";
+import { Body, Controller, HttpException, HttpStatus, Post, Req, Session } from "@nestjs/common";
 import { DiscordBotService } from "src/extension-modules/discord/discord-bot.service";
 import { UserService } from "../users/User.service";
 
@@ -7,13 +7,13 @@ export default class AuthController{
     constructor(private readonly discordService: DiscordBotService, private readonly userService: UserService){}
 
     @Post('user')
-    async userLogin(@Body('code') code: string){        
+    async userLogin(@Body('code') code: string, @Session() session: Express.Session){        
         const user = await this.discordService.getUserLogin(code).catch(() => {
             throw new HttpException('\'code\' é invalido.', HttpStatus.BAD_REQUEST)
         })
 
-        const userDb = await this.userService.login(user)
-        return userDb
-        
+        session.user = await this.userService.login(user)
+        session.save(console.error)
+        return session.id
     }
 }
