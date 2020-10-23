c6d12aabf6df1162bb367efc2685a2cacb47f6e8
Takasakiii <lucasmc2709@live.com>
2020-10-23 00:05:04 -0300

    remove typos

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,6 +1,4 @@
 import { Body, Controller, HttpException, HttpStatus, Post } from "@nestjs/common";
-import { DiscordBotService } from "src/extension-modules/discord/discord-bot.service";
-import { UserService } from "../users/User.service";
 import { AuthService } from "./auth.service";
 
 @Controller('auth')
@@ -9,7 +7,7 @@ export default class AuthController{
 
     @Post('user')
     async login(@Body('code') code: string){
-        const userLogged = await this.authService.validateUser(code).catch(err => {
+        const userLogged = await this.authService.validateUser(code).catch(() => {
             throw new HttpException('\'code\' se encontra invalido ou não foi definido.', HttpStatus.BAD_REQUEST)
         })
 
