8a087d805e3080fa41fcd12e14c38d7e06a122cb
Takasakiii <lucasmc2709@live.com>
2020-11-01 16:20:54 -0300

    auth desabilitado para aplicacoes desconhecidas

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,12 +1,17 @@
 import { Body, Controller, HttpException, HttpStatus, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { jwt } from '../../../config.json'
 
 @Controller('auth')
 export default class AuthController{
     constructor(private readonly authService: AuthService){}
 
     @Post('user')
-    async login(@Body('code') code: string){
+    async login(@Body('code') code: string, @Body('identify') identify: string){
+        if(jwt.authorized_clients.findIndex(x => x == identify) == -1)
+            throw new HttpException('The application is not authorized to use this endpoint.', HttpStatus.NOT_ACCEPTABLE)
+
+
         const userLogged = await this.authService.validateUser(code).catch(() => {
             throw new HttpException('\'code\' is invalid.', HttpStatus.BAD_REQUEST)
         })
