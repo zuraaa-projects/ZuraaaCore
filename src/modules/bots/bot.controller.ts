9583b5869980d7ea7d30a773b2f861e98872adc8
Gabriel-Paulucci <gabrielpauluccimunizmendes@gmail.com>
2021-03-03 21:13:44 -0300

    Modificado o metodo que estava na rota resetVotes

--- src/modules/bots/bot.controller.ts
+++ src/modules/bots/bot.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, HttpException, HttpStatus, Param, Post, UseGuards, Res, Delete, Header, Put } from '@nestjs/common'
+import { Controller, Get, HttpException, HttpStatus, Param, Post, UseGuards, Res, Delete, Header, Put, Patch } from '@nestjs/common'
 import { Body, Query, Req } from '@nestjs/common/decorators/http/route-params.decorator'
 import { BotService } from 'src/modules/bots/bot.service'
 import { SvgCreator } from 'src/utils/svg-creator'
@@ -37,7 +37,7 @@ export default class BotController {
     }
   }
 
-  @Put()
+  @Patch()
   @UseGuards(JwtAuthGuard)
   async updateAllBots (@Query('type') type: string, @Req() req: Express.Request): Promise<void> {
     const { role } = req.user as RequestUserPayload
