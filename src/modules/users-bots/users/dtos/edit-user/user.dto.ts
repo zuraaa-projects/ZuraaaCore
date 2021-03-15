a0bd28290d720a92e18e273b1744611a39340b74
KuryKat <57152799+KuryKat@users.noreply.github.com>
2021-03-15 13:01:20 -0300

    Agora sim não buga mais :D

--- src/modules/users-bots/users/dtos/edit-user/user.dto.ts
+++ src/modules/users-bots/users/dtos/edit-user/user.dto.ts
@@ -1,6 +1,7 @@
-import { Length } from 'class-validator'
+import { Length, IsOptional } from 'class-validator'
 
 export default class UserDto {
+  @IsOptional()
   @Length(1, 255)
   bio!: string | null
 }
