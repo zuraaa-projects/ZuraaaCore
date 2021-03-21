1bd1833fe7d44dd214f099aec6c31f77f2a09b36
Gabriel-Paulucci <gabrielpauluccimunizmendes@gmail.com>
2021-03-21 18:28:41 -0300

    Adicionado o not found

--- src/modules/avatars/avatar.controller.ts
+++ src/modules/avatars/avatar.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Header, Param, Res } from '@nestjs/common'
+import { Controller, Get, Header, HttpException, HttpStatus, Param, Res } from '@nestjs/common'
 import { Response } from 'express'
 import _ from 'lodash'
 import { DiscordBotService } from 'src/extension-modules/discord/discord-bot.service'
@@ -14,13 +14,18 @@ export default class AvatarController {
   @Get(':id')
   @Header('Cache-Control', 'public, max-age=60, only-if-cached')
   async avatar (@Param('id') id: string, @Res() res: Response): Promise<void> {
-    const image = await this.avatarsService.getAvatar(id)
-    if (image !== undefined) {
-      res.contentType(image.type)
-      res.send(image.data)
-    } else {
-      const user = await this.discordService.getUser(id)
-      res.redirect(`https://cdn.discordapp.com/embed/avatars/${_.toInteger(user.discriminator) % 5}.png`)
+    try {
+      const image = await this.avatarsService.getAvatar(id)
+      if (image != null) {
+        res.contentType(image.type)
+        res.send(image.data)
+      } else {
+        const user = await this.discordService.getUser(id)
+        res.redirect(`https://cdn.discordapp.com/embed/avatars/${_.toInteger(user.discriminator) % 5}.png`)
+      }
+    } catch (error) {
+      console.error('Not found')
+      throw new HttpException('Avatar not found', HttpStatus.NOT_FOUND)
     }
   }
 }
